5bdaa1d927981b8b7b7366182cec5287deec4ad6
richardsuhendra <richardsuhendra@users.noreply.github.com>
2020-11-10 22:54:11 -0500

    hi carter

---
+++ equations.py
@@ -0,0 +1,116 @@
+import numpy as np
+from scipy import sparse
+
+from field import Field, FieldSystem
+from timesteppers import CrankNicolson, PredictorCorrector
+from spatial import FiniteDifferenceUniformGrid, Left, Right
+
+class SW: # no bottom topography, coriolis effect, drag, viscocity, boundary conditions,
+
+    def __init__(self, X, spatial_order, g): # g is gravitational constant
+        h = X.field_list[0]
+        u = X.field_list[1]
+        v = X.field_list[2]
+        self.domain = u.domain
+        self.X = X
+
+        dhdx = FiniteDifferenceUniformGrid(1, spatial_order, h, axis=0)
+        dhdy = FiniteDifferenceUniformGrid(1, spatial_order, h, axis=1)
+        dudx = FiniteDifferenceUniformGrid(1, spatial_order, u, axis=0)
+        dudy =  FiniteDifferenceUniformGrid(1, spatial_order, u, axis=1)
+        dvdx = FiniteDifferenceUniformGrid(1, spatial_order, v, axis=0)
+        dvdy = FiniteDifferenceUniformGrid(1, spatial_order, v, axis=1)
+
+
+        self.F_ops = [-h*dudx - u*dhdx -h*dvdy -v*dhdy,
+                      -u*dudx -v*dudy -g*dhdx,
+                      -u*dvdx -v*dvdy -g*dhdy]
+
+
+class SW2:  # no viscocity, no boundary conditions,
+
+    def __init__(self, X, spatial_order, g, f,b): # g=gravity, f=coriolis, b=drag
+        h = X.field_list[0]
+        u = X.field_list[1]
+        v = X.field_list[2]
+        H = X.field_list[3]
+        self.domain = u.domain
+        self.X = X
+
+        dhdx = FiniteDifferenceUniformGrid(1, spatial_order, h, axis=0)
+        dhdy = FiniteDifferenceUniformGrid(1, spatial_order, h, axis=1)
+        dudx = FiniteDifferenceUniformGrid(1, spatial_order, u, axis=0)
+        dudy = FiniteDifferenceUniformGrid(1, spatial_order, u, axis=1)
+        dvdx = FiniteDifferenceUniformGrid(1, spatial_order, v, axis=0)
+        dvdy = FiniteDifferenceUniformGrid(1, spatial_order, v, axis=1)
+        dHdx = FiniteDifferenceUniformGrid(1, spatial_order, H, axis=0)
+        dHdy = FiniteDifferenceUniformGrid(1, spatial_order, H, axis=1)
+
+        self.F_ops = [-h * dudx - u * dhdx - h * dvdy - v * dhdy -H*dudx -H*dvdy - u*dHdx -v*dHdy,
+                      -u * dudx - v * dudy - g * dhdx + f*v -b*u,
+                      -u * dvdx - v * dvdy - g * dhdy - f*u -b*v,
+                      0*H]
+
+
+
+
+
+
+
+
+class SWsqBC: # SW with boundary conditions on a square tub
+
+    def __init__(self, X, spatial_order, g):
+        h = X.field_list[0]
+        u = X.field_list[1]
+        v = X.field_list[2]
+        self.domain = u.domain
+        self.X = X
+
+        dhdx = FiniteDifferenceUniformGrid(1, spatial_order, h, axis=0)
+        dhdy = FiniteDifferenceUniformGrid(1, spatial_order, h, axis=1)
+        dudx = FiniteDifferenceUniformGrid(1, spatial_order, u, axis=0)
+        dudy =  FiniteDifferenceUniformGrid(1, spatial_order, u, axis=1)
+        dvdx = FiniteDifferenceUniformGrid(1, spatial_order, v, axis=0)
+        dvdy = FiniteDifferenceUniformGrid(1, spatial_order, v, axis=1)
+
+
+        self.F_ops = [-h*dudx - u*dhdx -h*dvdy -v*dhdy,
+                      -u*dudx -v*dudy -g*dhdx,
+                      -u*dvdx -v*dvdy -g*dhdy]
+
+        self.BCs = [Left(0, spatial_order, u, 0, axis=0), Right(0, spatial_order, u, 0, axis=0),
+                    Left(0, spatial_order, u, 0, axis=1), Right(0, spatial_order, u, 0, axis=1),
+                    Left(0, spatial_order, v, 0, axis=0), Right(0, spatial_order, v, 0, axis=0),
+                    Left(0, spatial_order, v, 0, axis=1), Right(0, spatial_order, v, 0, axis=1)]
+
+
+class SW2sqBC: # SW2 with boundary conditions on a square tub
+    #implement bc for H, right now only works for periodic H, eg sinx*siny
+
+    def __init__(self, X, spatial_order, g, f, b):  # g=gravity, f=coriolis, b=drag
+        h = X.field_list[0]
+        u = X.field_list[1]
+        v = X.field_list[2]
+        H = X.field_list[3]
+        self.domain = u.domain
+        self.X = X
+
+        dhdx = FiniteDifferenceUniformGrid(1, spatial_order, h, axis=0)
+        dhdy = FiniteDifferenceUniformGrid(1, spatial_order, h, axis=1)
+        dudx = FiniteDifferenceUniformGrid(1, spatial_order, u, axis=0)
+        dudy = FiniteDifferenceUniformGrid(1, spatial_order, u, axis=1)
+        dvdx = FiniteDifferenceUniformGrid(1, spatial_order, v, axis=0)
+        dvdy = FiniteDifferenceUniformGrid(1, spatial_order, v, axis=1)
+        dHdx = FiniteDifferenceUniformGrid(1, spatial_order, H, axis=0)
+        dHdy = FiniteDifferenceUniformGrid(1, spatial_order, H, axis=1)
+
+        self.F_ops = [-h * dudx - u * dhdx - h * dvdy - v * dhdy - H * dudx - H * dvdy - u * dHdx - v * dHdy,
+                      -u * dudx - v * dudy - g * dhdx + f * v - b * u,
+                      -u * dvdx - v * dvdy - g * dhdy - f * u - b * v,
+                      0 * H]
+
+        self.BCs = [Left(0, spatial_order, u, 0, axis=0), Right(0, spatial_order, u, 0, axis=0),
+                    Left(0, spatial_order, u, 0, axis=1), Right(0, spatial_order, u, 0, axis=1),
+                    Left(0, spatial_order, v, 0, axis=0), Right(0, spatial_order, v, 0, axis=0),
+                    Left(0, spatial_order, v, 0, axis=1), Right(0, spatial_order, v, 0, axis=1)]
---
+++ spatial.py
@@ -0,0 +1,189 @@
+
+import numpy as np
+import scipy.sparse as sparse
+from scipy.special import factorial
+import math
+
+from field import UniformNonPeriodicGrid, LinearOperator
+
+
+# next two functions from K. J. Burns
+def axslice(axis, start, stop, step=None):
+    """Slice array along a specified axis."""
+    if axis < 0:
+        raise ValueError("`axis` must be positive")
+    slicelist = [slice(None)] * axis
+    slicelist.append(slice(start, stop, step))
+    return tuple(slicelist)
+
+def apply_matrix(matrix, array, axis, **kw):
+    """Contract any direction of a multidimensional array with a matrix."""
+    dim = len(array.shape)
+    # Build Einstein signatures
+    mat_sig = [dim, axis]
+    arr_sig = list(range(dim))
+    out_sig = list(range(dim))
+    out_sig[axis] = dim
+    # Handle sparse matrices
+    if sparse.isspmatrix(matrix):
+        matrix = matrix.toarray()
+    return np.einsum(matrix, mat_sig, array, arr_sig, out_sig, **kw)
+
+class FiniteDifferenceUniformGrid(LinearOperator):
+
+    def __init__(self, derivative_order, convergence_order, arg, axis=0, pad=None, stencil_type='centered'):
+        if stencil_type == 'centered' and convergence_order % 2 != 0:
+            raise ValueError("Centered finite difference has even convergence order")
+        if stencil_type == 'forward' or stencil_type == 'backward':
+            if derivative_order % 2 == 0:
+                raise ValueError("Forward and backward finite difference only for odd derivative order.")
+
+        self.derivative_order = derivative_order
+        self.convergence_order = convergence_order
+        self.stencil_type = stencil_type
+        self.pad = pad
+        self.axis = axis
+        self.grid = arg.domain.grids[axis]
+        self._stencil_shape(stencil_type)
+        self._make_stencil(self.grid)
+        self._build_matrices(self.grid)
+        super().__init__(arg)
+
+    def _stencil_shape(self, stencil_type):
+        dof = self.derivative_order + self.convergence_order
+
+        if stencil_type == 'centered':
+            # cancellation if derivative order is even
+            dof = dof - (1 - dof % 2)
+            j = np.arange(dof) - dof//2
+        if stencil_type == 'forward':
+            j = np.arange(dof) - dof//2 + 1
+        if stencil_type == 'backward':
+            j = np.arange(dof) - dof//2
+        if stencil_type == 'full forward':
+            j = np.arange(dof)
+        if stencil_type == 'full backward':
+            j = -np.arange(dof)
+
+        self.dof = dof
+        if self.pad == None:
+            self.pad = (-np.min(j), np.max(j))
+        self.j = j
+
+    def _make_stencil(self, grid):
+
+        # assume constant grid spacing
+        self.dx = grid.dx
+        i = np.arange(self.dof)[:, None]
+        j = self.j[None, :]
+        S = 1/factorial(i)*(j*self.dx)**i
+
+        b = np.zeros( self.dof )
+        b[self.derivative_order] = 1.
+
+        self.stencil = np.linalg.solve(S, b)
+
+    def _build_matrices(self, grid):
+        shape = [grid.N + self.pad[0] + self.pad[1]] * 2
+        padded_matrix = sparse.diags(self.stencil, self.j, shape=shape)
+        self.padded_matrix = sparse.diags(self.stencil, self.j, shape=shape)
+        self.matrix = self._unpadded_matrix(grid)
+        if isinstance(grid, UniformNonPeriodicGrid):
+            # modify derivative matrix
+            pass
+
+    def error_estimate(self, lengthscale):
+        error_degree = self.dof
+        if self.stencil_type == 'centered' and self.derivative_order % 2 == 0:
+            error_degree += 1
+        error = np.abs(np.sum( self.stencil*(self.j*self.dx/lengthscale)**error_degree ))
+        error *= 1/math.factorial(error_degree)
+        return error
+
+    def plot_matrix(self):
+        self._plot_2D(self.matrix.A)
+
+    def fourier_representation(self):
+        kh = np.linspace(-np.pi, np.pi, 100)
+        derivative = np.sum(self.stencil[:,None]*np.exp(1j*kh[None,:]*self.j[:,None]),axis=0)*self.dx**self.derivative_order
+        return kh, derivative
+
+
+class BoundaryCondition(LinearOperator):
+
+    def __init__(self, derivative_order, convergence_order, arg, value, axis=0):
+
+        self.derivative_order = derivative_order
+        self.convergence_order = convergence_order
+        self.dof = self.derivative_order + self.convergence_order
+        self.value = value
+        self.axis = axis
+        self.grid = arg.domain.grids[axis]
+        if not isinstance(self.grid, UniformNonPeriodicGrid):
+            raise ValueError("Can only apply BC's on UniformNonPeriodicGrid")
+        self._build_vector()
+        N = self.grid.N
+        self.matrix = self.vector.reshape((1,N))
+        super().__init__(arg)
+
+    def _coeffs(self, dx, j):
+        i = np.arange(self.dof)[:, None]
+        j = j[None, :]
+        S = 1/factorial(i)*(j*dx)**i
+
+        b = np.zeros( self.dof )
+        b[self.derivative_order] = 1.
+
+        return np.linalg.solve(S, b)
+
+    def operate(self, out=None):
+        self.data = self.matrix
+
+    def field_coeff(self, field, axis=None):
+        if axis == None:
+            axis = self.axis
+        if axis != self.axis:
+            raise ValueError("Axis must match self.axis")
+        if field == self.field:
+            return self.matrix
+        else:
+            return 0*self.matrix
+
+
+class Left(BoundaryCondition):
+
+    def _build_vector(self):
+        dx = self.grid.dx
+        j = np.arange(self.dof)+1/2
+
+        coeffs = self._coeffs(dx, j)
+
+        self.vector = np.zeros(self.grid.N)
+        self.vector[:self.dof] = coeffs
+
+    def operate(self):
+        slice = axslice(self.axis, 1, None)
+        BC = self.value - apply_matrix(self.matrix[:,1:], self.field.data[slice], self.axis)
+        BC /= self.matrix[0,0]
+        slice = axslice(self.axis, 0, 1)
+        self.field.data[slice] = BC
+
+
+class Right(BoundaryCondition):
+
+    def _build_vector(self):
+        dx = self.grid.dx
+        j = np.arange(self.dof) - self.dof + 1/2
+
+        coeffs = self._coeffs(dx, j)
+
+        self.vector = np.zeros(self.grid.N)
+        self.vector[-self.dof:] = coeffs
+
+    def operate(self, out=None):
+        slice = axslice(self.axis, None, -1)
+        BC = self.value - apply_matrix(self.matrix[:,:-1], self.field.data[slice], self.axis)
+        BC /= self.matrix[0,-1]
+        slice = axslice(self.axis, -1, None)
+        self.field.data[slice] = BC
+
