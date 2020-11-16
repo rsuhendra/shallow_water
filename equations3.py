065ee7c04ec5168f215d80e3961ab21a61e216e4
richardsuhendra <richardsuhendra@users.noreply.github.com>
2020-11-15 22:04:17 -0500

    hello

---
+++ equations3.py
@@ -0,0 +1,26 @@
+import numpy as np
+from scipy import sparse
+
+from field import Field, FieldSystem
+from timesteppers import CrankNicolson, PredictorCorrector
+from spatial import FiniteDifferenceUniformGrid, Left, Right
+
+class linearSW:  # no viscocity, boundary conditions,
+
+    def __init__(self, X, spatial_order, g, f,H): # g=gravity, f=coriolis, b=drag
+        h = X.field_list[0]
+        u = X.field_list[1]
+        v = X.field_list[2]
+
+        self.domain = u.domain
+        self.X = X
+
+        dhdx = FiniteDifferenceUniformGrid(1, spatial_order, h, axis=0)
+        dhdy = FiniteDifferenceUniformGrid(1, spatial_order, h, axis=1)
+        dudx = FiniteDifferenceUniformGrid(1, spatial_order, u, axis=0)
+        dvdy = FiniteDifferenceUniformGrid(1, spatial_order, v, axis=1)
+
+        self.F_ops = [-H*dudx -H*dvdy,
+                      - g * dhdx + f*v ,
+                      - g * dhdy - f*u]
+        self.BCs=[]
